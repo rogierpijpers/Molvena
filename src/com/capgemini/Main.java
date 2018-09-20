60824ae5285e05097488a96a615ea9e4407edabc
griftj <jeroen.grift@kadaster.nl>
2018-09-20 16:04:50 +0200

    main nothing

--- src/com/capgemini/Main.java
+++ src/com/capgemini/Main.java
@@ -1,8 +1,5 @@
 package com.capgemini;
 
-import com.capgemini.data.RoomRepository;
-import com.capgemini.domain.Room;
-
 public class Main{
 	public static void main(String[] args){ System.out.println("Test.");
 	}
