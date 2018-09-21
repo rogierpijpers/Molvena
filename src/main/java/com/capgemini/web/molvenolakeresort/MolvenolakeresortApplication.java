858e5547a27b2915e9e826b107644554de7aa846
Jeroen Knoester <jeroen.knoester@capgemini.com>
2018-09-21 15:40:39 +0200

    add springboot to molveno

---
+++ src/main/java/com/capgemini/web/molvenolakeresort/MolvenolakeresortApplication.java
@@ -0,0 +1,12 @@
+package com.capgemini.web.molvenolakeresort;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MolvenolakeresortApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MolvenolakeresortApplication.class, args);
+	}
+}
