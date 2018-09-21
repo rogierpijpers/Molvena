cf7788e9e2d1fa21ad6f7e8bf2530b327611e6b9
Go <cheston.go@capgemini.com>
2018-09-21 14:31:04 +0200

    getAllReservations returns non-deleted reservations, Added getAllSoftDeletedReservations

--- src/com/capgemini/data/ReservationRepository.java
+++ src/com/capgemini/data/ReservationRepository.java
@@ -8,13 +8,34 @@ import java.util.List;
 public class ReservationRepository {
 
     private List<Reservation> reservations = new ArrayList<>();
+    private List<Reservation> nonSoftDeletedReservations = new ArrayList<>();
+    private List<Reservation> SoftDeletedReservations = new ArrayList<>();
 
     public void addReservation(Reservation reservation) {
         reservation.setReservationID((reservations.size() + 1));
         reservations.add(reservation);
     }
 
+
     public List<Reservation> getAllReservations() {
-        return reservations;
+        for (Reservation reservation :
+                reservations) {
+
+            if (!reservation.isDeleted()) {
+                nonSoftDeletedReservations.add(reservation);
+            }
+        }
+        return nonSoftDeletedReservations;
     }
+    public List<Reservation> getAllSoftDeletedReservations() {
+        for (Reservation reservation :
+                reservations) {
+
+            if (reservation.isDeleted()) {
+                SoftDeletedReservations.add(reservation);
+            }
+        }
+        return SoftDeletedReservations;
+    }
+
 }
