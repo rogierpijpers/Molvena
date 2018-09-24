6f4719184560c03243e148291758b7207ddac455
Unknown <rogierpijpers1992@gmail.com>
2018-09-24 14:51:47 +0200

    Changed package names

--- src/main/java/com/capgemini/web/MolvenolakeresortApplication.java
+++ src/main/java/com/capgemini/web/MolvenolakeresortApplication.java
@@ -1,4 +1,4 @@
-package com.capgemini.web.molvenolakeresort;
+package com.capgemini.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
