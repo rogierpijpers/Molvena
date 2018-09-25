fda6421f7f8355861891825272a8c2f18d1a1443
Thomvdm <Thom@moosjes.nl>
2018-09-25 13:38:30 +0200

    Creates PersonRepo, updated WebSecurityConfig

--- src/main/java/com/capgemini/web/authentication/WebSecurityConfig.java
+++ src/main/java/com/capgemini/web/authentication/WebSecurityConfig.java
@@ -1,6 +1,7 @@
 package com.capgemini.web.authentication;
 
 
+import com.capgemini.data.PersonRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -13,11 +14,16 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     private static final String ENCODED_PASSWORD = "$2a$10$AIUufK8g6EFhBcumRRV2L.AQNz3Bjp7oDQVFiO5JJMBFZQ6x2/R/2";
+    private PersonRepository personRepository = new PersonRepository();
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        personRepository.addInitialAccounts();
+        String username = "Thom@moosjes.nl";
+        String password = personRepository.getSinglePerson("Thom@moosjes.nl").getPassword();
+
         auth.inMemoryAuthentication().passwordEncoder(passwordEncoder())
-                .withUser("user").password(ENCODED_PASSWORD).roles("USER");
+                  .withUser(username).password(password).roles("USER");
     }
 
     @Bean
