289cb237b20c4c700005f17572b7eb47640d36ea
Pijpers <rogierpijpers1992@gmail.com>
2018-09-27 09:28:39 +0200

    Added dummy (failing) for endpoint testing

--- src/test/java/com/capgemini/web/ReservationControllerTest.java
+++ src/test/java/com/capgemini/web/ReservationControllerTest.java
@@ -1,16 +1,34 @@
 package com.capgemini.web;
 
 import org.junit.Test;
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 public class ReservationControllerTest {
 
+    @Autowired
+    private MockMvc mockMvc;
+
     @Test
-    public void testGetReservationsForGuest(){
+    public void testGetReservationsForGuest() throws Exception{
+
+        this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk())
+                .andExpect(content().string(containsString("Hello World")));
+
+
         //TODO: get all reservations for a logged in guest
     }
 
