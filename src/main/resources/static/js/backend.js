e11894740c715d5481b0cbdd403290a7b46f11b1
Thomvdm <Thom@moosjes.nl>
2018-09-27 17:24:03 +0200

    updates.....

--- src/main/resources/static/js/backend.js
+++ src/main/resources/static/js/backend.js
@@ -1,5 +1,5 @@
 //var urlReservation = "http://localhost:8080/reservation/";
-var urlReservation = "http://www.mocky.io/v2/5bacd2c33300002a000eb39b";
+var urlReservation = "http://localhost:8080/reservation/";
 var urlAccount = "http://localhost:8080/account/";
 
 function getSingleReservationByString(id){
@@ -12,8 +12,9 @@ function getSingleReservationByString(id){
 				for (reservations in json){
 					if (id == json[reservations]["guest"]["mail"]){
 						//id = json[reservations]["reservationID"];
-						document.getElementById("reservationDisplayText").innerHTML = JSON.stringify(json[reservations]);
-
+						//document.getElementById("reservationDisplayText").innerHTML = JSON.stringify(json[reservations]);
+						prettifyJSON(json, "#reservationDisplayText");
+						return json;
 					}
 				}			
 			});
@@ -28,13 +29,15 @@ function getSingleReservationById(id){
 	.then(function(json){
 		for (reservations in json){
 			if (id == json[reservations]["reservationID"]){
-						document.getElementById("reservationDisplayText").innerHTML = JSON.stringify(json[reservations]);
+						//document.getElementById("reservationDisplayText").innerHTML = JSON.stringify(json[reservations]);
+						prettifyJSON(json, "#reservationDisplayText");
+						return json;
 					}
 				}	
 			});
 }
 
-// Only works for 1 reservatin per person now
+// Only works for 1 reservation per person now
 function getSingleReservation(){
 	var id = document.querySelector("#inputResId").value;
 
@@ -59,61 +62,72 @@ function getAllReservations(){
 	.then(function(json){
 		console.log(JSON.stringify(json));
 		// TODO: display in html
-		document.querySelector("#reservationDisplayText").innerHTML = JSON.stringify(json);
+		document.querySelector("#allReservationDisplayText").innerHTML = JSON.stringify(json);
+		//prettifyJSON(json, "#");
 	});
 }
 
 function createReservation(){
-	var firstName = document.querySelector("#inputFirstNameCreate").value;
-	var lastName = document.querySelector("#inputFirstNameCreate").value;
-	var mail = document.querySelector("#inputMailCreate").value;
-	var startDate = document.querySelector("#inputStartDateCreate").value;
-	var endDate = document.querySelector("#inputEndDateCreate").value;
-	var amountOfGuests = document.querySelector("#inputAmountOfGuestsCreate").value;
-	
-	// TODO: guest, room and roomtype objects
-	//var guest = document.querySelector("").value;
-	//var room = document.querySelector("#inputRoomCreate").value;
-	//var roomType = document.querySelector("#inputRoomTypeCreate").value;
+	var firstName = document.querySelector("input[name='inputFirstNameCreate']").value;
+	var lastName = document.querySelector("input[name='inputLastNameCreate']").value;
+	var mail = document.querySelector("input[name='inputMailCreate']").value;
+	//var startDate = document.querySelector("input[name='inputStartDateCreate']").value;
+	//var endDate = document.querySelector("input[name='inputEndDateCreate']").value;
+	startDate = "1969-12-31T23:59:58.029+0000"; 
+	endDate = "1979-12-31T23:59:58.029+0000";
+	var amountOfGuests = document.querySelector("input[name='inputAmountOfGuestsCreate']").value;
+	var postRequest = { 
+		        "startDate" : startDate,
+		        "endDate" : endDate,
+		        "amountOfGuests" : amountOfGuests,
+			        "guest": {
+						"firstName": firstName,
+						"lastName": lastName,
+						"password": "$2a$10$AIUufK8g6EFhBcumRRV2L.AQNz3Bjp7oDQVFiO5JJMBFZQ6x2/R/2",
+						"dateOfBirth": "1969-12-31T23:59:58.029+0000",
+						"mail": mail,
+						"phone": "123456789",
+						"address": "Straat 1",
+						"city": "Veldhoven",
+						"zipCode": "5555LL",
+						"country": "NL",
+						"role": "ROLE_GUEST"
+					},
+					"room": {
+						"roomID": 0,
+						"roomType": {
+							"singleBeds": 0,
+							"doubleBeds": 2
+						}
+					},
+					"roomType": {
+						"singleBeds": 0,
+						"doubleBeds": 2
+					}
+};
 
-	var reservationJSON = "{startDate: " + startDate + ", endDate: " + endDate + ", amountOfGuests: " + amountOfGuests + "}";
-
-	console.log("Creating reservation with json: " + reservationJSON);
-	var newReservation = JSON.parse(reservationJSON);
-
+	var postRequestStringifyd = JSON.stringify(postRequest);
+	console.log("Creating reservation with json: " + postRequestStringifyd);
 	fetch(urlReservation, {
 		method: "POST",
-		body: JSON.stringify(reservationJSON),
+		body: postRequestStringifyd,
 		headers:{
 			'Content-Type': 'application/json'
 		}
-	}).then(res => res.json())
-	.then(response => console.log('Success:', JSON.stringify(response)))
+	})//.then(res => res.json())
+	.then(response => console.log('Succes:', JSON.stringify(response)))
 	.catch(error => console.error('Error:', error));
 }
 
 function updateReservation(){
-	var id = document.querySelector("#inputIdUpdate").value;
-	var startDate = document.querySelector("#inputStartDateUpdate").value;
-	var endDate = document.querySelector("#inputEndDateUpdate").value;
-	var amountOfGuests = document.querySelector("#inputAmountOfGuestsUpdate").value;
+	var id = document.querySelector("input[name='inputIdUpdate']").value;
+	íf (typeof id === "string"){
+		//	get
+	}
+	else if ($.isNumeric(id)){
 
-	// TODO: guest, room and roomtype objects
-	var guest = document.querySelector("").value;
-	var room = document.querySelector("#inputRoomUpdate").value;
-	var roomType = document.querySelector("#inputRoomTypeUpdate").value;
+	}
 
-	var reservationJSON = "{ id: " + id + ", startDate: " + startDate + ", endDate: " + endDate + ", amountOfGuests: " + amountOfGuests + "}";
-
-	console.log("Updating reservation with id " + id);
-	fetch(urlReservation + id, {
-		method: "PUT",
-		body: JSON.stringify(reservationJSON),
-		headers:{
-			'Content-Type': 'application/json'
-		}
-	})
-	.then(response => response.json());
 }
 
 function deleteReservation(){
--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -42,25 +42,40 @@ function toggleItems(){
 // TODO: Alles in 1 nette functie
 function toggleGetReservation(){
 	document.querySelector(".getReservationDisplay").style.display = "block";
+	document.querySelector(".getAllReservationsDisplay").style.display = "none";
 	document.querySelector(".createReservationDisplay").style.display = "none";
 	document.querySelector(".updateReservationDisplay").style.display = "none";
 	document.querySelector(".deleteReservationDisplay").style.display = "none";
 }
 
+function toggleAllReservations(){
+	document.querySelector(".getReservationDisplay").style.display = "none";
+	document.querySelector(".getAllReservationsDisplay").style.display = "block";
+	document.querySelector(".createReservationDisplay").style.display = "none";
+	document.querySelector(".updateReservationDisplay").style.display = "none";
+	document.querySelector(".deleteReservationDisplay").style.display = "none";
+
+	getAllReservations();
+}
+
 function toggleCreateReservation(){
 	document.querySelector(".getReservationDisplay").style.display = "none";
+	document.querySelector(".getAllReservationsDisplay").style.display = "none";
 	document.querySelector(".createReservationDisplay").style.display = "block";
 	document.querySelector(".updateReservationDisplay").style.display = "none";
 	document.querySelector(".deleteReservationDisplay").style.display = "none";
 }
 function toggleUpdateReservation(){
 	document.querySelector(".getReservationDisplay").style.display = "none";
+	document.querySelector(".getAllReservationsDisplay").style.display = "none";
 	document.querySelector(".createReservationDisplay").style.display = "none";
 	document.querySelector(".updateReservationDisplay").style.display = "block";
 	document.querySelector(".deleteReservationDisplay").style.display = "none";
 }
+
 function toggleDeleteReservation(){
 	document.querySelector(".getReservationDisplay").style.display = "none";
+	document.querySelector(".getAllReservationsDisplay").style.display = "none";
 	document.querySelector(".createReservationDisplay").style.display = "none";
 	document.querySelector(".updateReservationDisplay").style.display = "none";
 	document.querySelector(".deleteReservationDisplay").style.display = "block";
