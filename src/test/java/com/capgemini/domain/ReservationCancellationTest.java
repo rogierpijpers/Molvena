1a155d09c220355284b22abb4a30a8c42e184ac0
Unknown <rogierpijpers1992@gmail.com>
2018-10-08 15:19:56 +0200

    Added unit testing for domain class

---
+++ src/test/java/com/capgemini/domain/ReservationCancellationTest.java
@@ -0,0 +1,68 @@
+package com.capgemini.domain;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Calendar;
+import java.util.Date;
+
+public class ReservationCancellationTest {
+
+    private Date createDateMinus(int amount){
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(new Date());
+        cal.add(Calendar.DATE, -amount);
+        Date dateMinusAmount = cal.getTime();
+        return dateMinusAmount;
+    }
+
+    @Test
+    public void testCancellationC0Percent(){
+        Date date = createDateMinus(21);
+
+        ReservationCancellation cancellation = new ReservationCancellation(date);
+        cancellation.setChargeCancellationConditions(true);
+
+        Assert.assertEquals(0, cancellation.getChargedPercentage(), 0.001);
+    }
+
+    @Test
+    public void testCancellation25Percent(){
+        Date date = createDateMinus(20);
+
+        ReservationCancellation cancellation = new ReservationCancellation(date);
+        cancellation.setChargeCancellationConditions(true);
+
+        Assert.assertEquals(25, cancellation.getChargedPercentage(), 0.001);
+    }
+
+    @Test
+    public void testCancellation50Percent(){
+        Date date = createDateMinus(11);
+
+        ReservationCancellation cancellation = new ReservationCancellation(date);
+        cancellation.setChargeCancellationConditions(true);
+
+        Assert.assertEquals(50, cancellation.getChargedPercentage(), 0.001);
+    }
+
+    @Test
+    public void testCancellation100Percent(){
+        Date date = createDateMinus(10);
+
+        ReservationCancellation cancellation = new ReservationCancellation(date);
+        cancellation.setChargeCancellationConditions(true);
+
+        Assert.assertEquals(100, cancellation.getChargedPercentage(), 0.001);
+    }
+
+    @Test
+    public void testCancellationhargeFree(){
+        Date date = createDateMinus(2);
+
+        ReservationCancellation cancellation = new ReservationCancellation(date);
+        cancellation.setChargeCancellationConditions(false);
+
+        Assert.assertEquals(0, cancellation.getChargedPercentage(), 0.001);
+    }
+}
