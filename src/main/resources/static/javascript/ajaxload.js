cceb9d1f3d801e5c462965d0baff54747c0b11ce
jeroengrift <jeroengrift@outlook.com>
2018-10-10 14:30:03 +0200

    removed dashboard, ajax laten we zitten

---
+++ src/main/resources/static/javascript/ajaxload.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+
+    $("#vullen").load("html/hotelbuilding.html");
+
+    $("#roommanagement").click(function() {
+    $("#vullen").load("html/hotelbuilding.html");
+    });
+
+    $("#reservationmanagement").click(function() {
+    $("#vullen").load("html/hotelreservation.html");
+    });
+
+    $("#accountmanagement").click(function() {
+    $("#vullen").load("html/hotelaccount.html");
+    });
+    });
